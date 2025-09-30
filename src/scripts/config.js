95bace9b166769e84ff34f861f8dc719c600506c
bintang25a <bintangalfizar25@gmail.com>
2025-09-30 11:01:16 +0700

    Starter Development

---
+++ src/scripts/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  BASE_URL: "API_BASE_URL",
+  ACCESS_TOKEN_KEY: "accessToken",
+  MAP_SERVICE_API_KEY: "5KyA0RLyaKp1W7RZusGI",
+};
+
+export default CONFIG;
---
+++ src/scripts/data/api.js
@@ -0,0 +1,102 @@
+import CONFIG from "../config";
+
+const ENDPOINTS = {
+  REGISTER: `${CONFIG.BASE_URL}/register`,
+  LOGIN: `${CONFIG.BASE_URL}/login`,
+
+  STORIES: `${CONFIG.BASE_URL}/stories`,
+};
+
+export async function setRegister(data) {
+  try {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    const response = await fetch(ENDPOINTS.REGISTER, options);
+    const responseJson = await response.json();
+    showResponseMessage(responseJson.message);
+  } catch (error) {
+    showResponseMessage(error);
+  }
+}
+
+export async function setLogin(data) {
+  try {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    const response = await fetch(ENDPOINTS.LOGIN, options);
+    const responseJson = await response.json();
+
+    localStorage.setItem("loginResult", JSON.stringify(response.loginResult));
+
+    showResponseMessage(responseJson.message);
+  } catch (error) {
+    showResponseMessage(error);
+  }
+}
+
+export async function getStories() {
+  try {
+    const response = await fetch(ENDPOINTS.STORIES);
+    const responseJson = await response.json();
+
+    if (responseJson.error) {
+      showResponseMessage(responseJson.message);
+    } else {
+      return responseJson.listStory;
+    }
+  } catch (error) {
+    showResponseMessage(error);
+  }
+}
+
+export async function getDetailStory(id) {
+  try {
+    const response = await fetch(`${ENDPOINTS.STORIES}/${id}`);
+    const responseJson = await response.json();
+
+    if (responseJson.error) {
+      showResponseMessage(responseJson.message);
+    } else {
+      return responseJson.story;
+    }
+  } catch (error) {
+    showResponseMessage(error);
+  }
+}
+
+export async function setStory(data) {
+  try {
+    const userData = JSON.parse(localStorage.getItem("loginResult"));
+    const token = userData.token;
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(data),
+    };
+
+    const response = await fetch(ENDPOINTS.STORIES, options);
+    const responseJson = await response.json();
+    showResponseMessage(responseJson.message);
+  } catch (error) {
+    showResponseMessage(error);
+  }
+}
+
+const showResponseMessage = (message = "Check your internet connection") => {
+  alert(message);
+};
